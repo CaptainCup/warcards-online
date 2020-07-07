84d504b7357e6b067c0e7ec264dc6c2b836331b6
CaptainCup <51924351+CaptainCup@users.noreply.github.com>
2020-07-07 17:52:39 +0800

    Updates

---
+++ precache-manifest.773e48468e51e3aaa8382fda5b1469cb.js
@@ -0,0 +1,310 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91ccd2422cd1d98b14109288385dc01",
+    "url": "/warcards-online/index.html"
+  },
+  {
+    "revision": "33a5862a60316f764902",
+    "url": "/warcards-online/static/css/main.80f20322.chunk.css"
+  },
+  {
+    "revision": "56cba373b068e2431358",
+    "url": "/warcards-online/static/js/2.52ea6a7d.chunk.js"
+  },
+  {
+    "revision": "104de647f2ec10ec6023",
+    "url": "/warcards-online/static/js/3.7a8af7c3.chunk.js"
+  },
+  {
+    "revision": "519c717ae925b06bda3d",
+    "url": "/warcards-online/static/js/4.04f4110c.chunk.js"
+  },
+  {
+    "revision": "cba062760feea26d4aa1",
+    "url": "/warcards-online/static/js/5.9696120c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/warcards-online/static/js/5.9696120c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a5862a60316f764902",
+    "url": "/warcards-online/static/js/main.c9327724.chunk.js"
+  },
+  {
+    "revision": "a278997b4d67e66003b3",
+    "url": "/warcards-online/static/js/runtime-main.06680369.js"
+  },
+  {
+    "revision": "408e2818f83b1aea9422893e9cf4232e",
+    "url": "/warcards-online/static/media/1.408e2818.jpg"
+  },
+  {
+    "revision": "efc4aa867a274d5de03d940a649e86a2",
+    "url": "/warcards-online/static/media/2.efc4aa86.jpg"
+  },
+  {
+    "revision": "5caaa988ec0c8820998edc574437b7af",
+    "url": "/warcards-online/static/media/3.5caaa988.jpg"
+  },
+  {
+    "revision": "7c77f54acb360322fe7a606c12eefb0c",
+    "url": "/warcards-online/static/media/4.7c77f54a.jpg"
+  },
+  {
+    "revision": "ab72e1928340646dbaab42f60c8afdc2",
+    "url": "/warcards-online/static/media/5.ab72e192.jpg"
+  },
+  {
+    "revision": "dc9b94b435af457e5a93206c3ff9dc1f",
+    "url": "/warcards-online/static/media/6.dc9b94b4.jpg"
+  },
+  {
+    "revision": "9534b63d911696f51c33045a7bdb03f6",
+    "url": "/warcards-online/static/media/7.9534b63d.jpg"
+  },
+  {
+    "revision": "eb3db90c43d16d52459c4c6282eaa564",
+    "url": "/warcards-online/static/media/8.eb3db90c.jpg"
+  },
+  {
+    "revision": "9b6899a841e0b842ca900e0bafbf3e3a",
+    "url": "/warcards-online/static/media/archmage.9b6899a8.jpg"
+  },
+  {
+    "revision": "c5860a361b824ff6cf170d8d80c73b38",
+    "url": "/warcards-online/static/media/armor.c5860a36.png"
+  },
+  {
+    "revision": "aaccbbb169dd7896355113468edc5341",
+    "url": "/warcards-online/static/media/attack.aaccbbb1.png"
+  },
+  {
+    "revision": "85897104660fb73b06d7869cf2bb794c",
+    "url": "/warcards-online/static/media/background.85897104.jpg"
+  },
+  {
+    "revision": "fb1f747efe28269ac9975ec590cbed26",
+    "url": "/warcards-online/static/media/blade-storm.fb1f747e.png"
+  },
+  {
+    "revision": "6d08e2c3a3bdc91f6d90894fba2955a3",
+    "url": "/warcards-online/static/media/blizzard.6d08e2c3.png"
+  },
+  {
+    "revision": "e87f78b54e34c0eb670eb7ec4f0215c6",
+    "url": "/warcards-online/static/media/bloodlust.e87f78b5.png"
+  },
+  {
+    "revision": "eb1a31468c67dc8ee304316755c19082",
+    "url": "/warcards-online/static/media/brick-border.eb1a3146.jpg"
+  },
+  {
+    "revision": "cdccc63f3dccfb4bc77869e0944a9de2",
+    "url": "/warcards-online/static/media/bricks_background.cdccc63f.jpg"
+  },
+  {
+    "revision": "016ae9a187e5ddec9f5d902ea5884174",
+    "url": "/warcards-online/static/media/captain.016ae9a1.png"
+  },
+  {
+    "revision": "6e12f61613ed36827ef6b38b01cec9c6",
+    "url": "/warcards-online/static/media/card.6e12f616.png"
+  },
+  {
+    "revision": "28ec2e44349a37d79a83c70c1b7d6b47",
+    "url": "/warcards-online/static/media/chain.28ec2e44.png"
+  },
+  {
+    "revision": "42e2d250ca9a03f5cfd767d98f4c7258",
+    "url": "/warcards-online/static/media/create-item.42e2d250.png"
+  },
+  {
+    "revision": "fb20b5604d34d15430b3db356fe98fb4",
+    "url": "/warcards-online/static/media/critical-strike.fb20b560.png"
+  },
+  {
+    "revision": "0bdd0699e8880975a9746c76a7d60d6a",
+    "url": "/warcards-online/static/media/dmg-1.0bdd0699.png"
+  },
+  {
+    "revision": "b2d4e5ff784e8f0ce74ad0f5b5a551d1",
+    "url": "/warcards-online/static/media/dmg-2.b2d4e5ff.png"
+  },
+  {
+    "revision": "7c85ca321eea1119b87a1561b05f9e1c",
+    "url": "/warcards-online/static/media/dmg-3.7c85ca32.png"
+  },
+  {
+    "revision": "e1bf3c9a018c86dded4780118b540711",
+    "url": "/warcards-online/static/media/dmg-4.e1bf3c9a.png"
+  },
+  {
+    "revision": "e5525ee5100495d7043ed14755e61d8f",
+    "url": "/warcards-online/static/media/enrage.e5525ee5.png"
+  },
+  {
+    "revision": "5b1861bac79cb4cd444d504a9b07c7c9",
+    "url": "/warcards-online/static/media/font.5b1861ba.otf"
+  },
+  {
+    "revision": "c7526450818342317c27a811bc74c09c",
+    "url": "/warcards-online/static/media/footman.c7526450.jpg"
+  },
+  {
+    "revision": "137cc7e72d115ff8898571c5948a6960",
+    "url": "/warcards-online/static/media/grom.137cc7e7.jpg"
+  },
+  {
+    "revision": "da9d335966d72b1897daa876f48bef47",
+    "url": "/warcards-online/static/media/gryphon.da9d3359.jpg"
+  },
+  {
+    "revision": "da4a8fe6e089e6072c82257af7a57aec",
+    "url": "/warcards-online/static/media/gyro.da4a8fe6.png"
+  },
+  {
+    "revision": "9213bb6d286b9878e3fe00409b9cac8b",
+    "url": "/warcards-online/static/media/hawk.9213bb6d.png"
+  },
+  {
+    "revision": "09603356ce11b7476485e22ffc50f9ed",
+    "url": "/warcards-online/static/media/header_menu.09603356.jpg"
+  },
+  {
+    "revision": "ca708afb681d06e8b156b943a77b8661",
+    "url": "/warcards-online/static/media/health.ca708afb.png"
+  },
+  {
+    "revision": "758296c5de6f7f6384aed03470155e7d",
+    "url": "/warcards-online/static/media/hp-big.758296c5.png"
+  },
+  {
+    "revision": "535e2e5ace34235bfc3ebf8ee7716915",
+    "url": "/warcards-online/static/media/hp-medium.535e2e5a.png"
+  },
+  {
+    "revision": "47fbd06d2e32dc8fc619c084591dac68",
+    "url": "/warcards-online/static/media/hp-small.47fbd06d.png"
+  },
+  {
+    "revision": "614637fc089bb6dd41709174f49b5e6d",
+    "url": "/warcards-online/static/media/jaina.614637fc.jpg"
+  },
+  {
+    "revision": "8a4105dfdd0b57932a471252bc70c14f",
+    "url": "/warcards-online/static/media/knight.8a4105df.png"
+  },
+  {
+    "revision": "e5b39b455a307e141b192d27a790b270",
+    "url": "/warcards-online/static/media/mana-big.e5b39b45.png"
+  },
+  {
+    "revision": "6e5ae1aec16c15d7b0db5ecd9f03c34f",
+    "url": "/warcards-online/static/media/mana-medium.6e5ae1ae.png"
+  },
+  {
+    "revision": "ce8178b757ec5450ed41df4582981605",
+    "url": "/warcards-online/static/media/mana-small.ce8178b7.png"
+  },
+  {
+    "revision": "5e209d8df46443334d677743d1ce20f9",
+    "url": "/warcards-online/static/media/map1.5e209d8d.png"
+  },
+  {
+    "revision": "d33569a5d1c9d5f03ef714222f255a2c",
+    "url": "/warcards-online/static/media/maxhp-big.d33569a5.png"
+  },
+  {
+    "revision": "b54386bdde6a65211942a77c4582dbb7",
+    "url": "/warcards-online/static/media/maxhp-small.b54386bd.png"
+  },
+  {
+    "revision": "264c9db95dad2aa18cd329059d3a0bd9",
+    "url": "/warcards-online/static/media/maxmana-big.264c9db9.png"
+  },
+  {
+    "revision": "ec087a5290c5481741c5b38f732afdcf",
+    "url": "/warcards-online/static/media/maxmana-small.ec087a52.png"
+  },
+  {
+    "revision": "d8057c357761b0796e8835f585d63cd3",
+    "url": "/warcards-online/static/media/menu_arrow.d8057c35.png"
+  },
+  {
+    "revision": "5558eb6c07f83bce0eb8002f02abe7eb",
+    "url": "/warcards-online/static/media/militia.5558eb6c.png"
+  },
+  {
+    "revision": "b19605ac67d0364c1029d074f2116449",
+    "url": "/warcards-online/static/media/mortar.b19605ac.png"
+  },
+  {
+    "revision": "703ffe0d0edfb8862db39e8544f955fb",
+    "url": "/warcards-online/static/media/mountain_king.703ffe0d.jpg"
+  },
+  {
+    "revision": "c517b2c8d2e07357ec77d824654f2ee8",
+    "url": "/warcards-online/static/media/open_cards.c517b2c8.jpg"
+  },
+  {
+    "revision": "f595991a92ce90e2bbbb3f0853cd901d",
+    "url": "/warcards-online/static/media/paladin.f595991a.png"
+  },
+  {
+    "revision": "627fe633f844097b4f73a27210f8b1c6",
+    "url": "/warcards-online/static/media/peasant.627fe633.jpg"
+  },
+  {
+    "revision": "6d9471c6394eeb4f926d61e1bb06a56d",
+    "url": "/warcards-online/static/media/priest.6d9471c6.png"
+  },
+  {
+    "revision": "9ccd1f2dce2cc15e73883f9b47b283ff",
+    "url": "/warcards-online/static/media/rejuv-big.9ccd1f2d.png"
+  },
+  {
+    "revision": "1e5036ad7cc2d04f84806203584a2e5b",
+    "url": "/warcards-online/static/media/rejuv-medium.1e5036ad.png"
+  },
+  {
+    "revision": "8751f91790a693affc5286241300798d",
+    "url": "/warcards-online/static/media/rejuv-small.8751f917.png"
+  },
+  {
+    "revision": "2ba727f0c395be023ac22d6a78faf348",
+    "url": "/warcards-online/static/media/restore.2ba727f0.png"
+  },
+  {
+    "revision": "a067012e8fe65b3fc2106a441b4ccda0",
+    "url": "/warcards-online/static/media/rifle.a067012e.jpg"
+  },
+  {
+    "revision": "82186a8d47f2baa359e2dc74e664c7cf",
+    "url": "/warcards-online/static/media/seller_img.82186a8d.jpg"
+  },
+  {
+    "revision": "4462abf4a4fcf802b4c1d74ceae28bd5",
+    "url": "/warcards-online/static/media/shop_background.4462abf4.jpg"
+  },
+  {
+    "revision": "fec92386bfddaaa0bd59143cf6c42887",
+    "url": "/warcards-online/static/media/shop_top_background.fec92386.jpg"
+  },
+  {
+    "revision": "e571ea8cc4a04f082b80bd1ab127f26d",
+    "url": "/warcards-online/static/media/sorceress.e571ea8c.jpg"
+  },
+  {
+    "revision": "1e17837fc39b90a08f70dfe373ec99bb",
+    "url": "/warcards-online/static/media/spell-breaker.1e17837f.png"
+  },
+  {
+    "revision": "c0b25331ed461a8377e71b93c092db01",
+    "url": "/warcards-online/static/media/tank.c0b25331.png"
+  },
+  {
+    "revision": "e6774f802239772fe6a3049b40288823",
+    "url": "/warcards-online/static/media/template.e6774f80.png"
+  }
+]);
